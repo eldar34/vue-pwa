1cf0ecc93133c06b4465f465c6811e63d37e8737
eldar <eldar@example.com>
2021-03-15 07:10:22 +0300

    deploy

---
+++ precache-manifest.f6c462ff55c5dc8349e31cfdd0ec7fc0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56e811e7100a8132e0c",
+    "url": "/vue-pwa/css/app.b229284b.css"
+  },
+  {
+    "revision": "3a31b638dad0889f2d95",
+    "url": "/vue-pwa/css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "ce1f8df0d96b974a22324264f4d13533",
+    "url": "/vue-pwa/img/logo.ce1f8df0.png"
+  },
+  {
+    "revision": "31431617a993910bb4a3830283d16539",
+    "url": "/vue-pwa/index.html"
+  },
+  {
+    "revision": "79a09fa328c00dee1f34",
+    "url": "/vue-pwa/js/about.154d5196.js"
+  },
+  {
+    "revision": "c56e811e7100a8132e0c",
+    "url": "/vue-pwa/js/app.70d8b4e6.js"
+  },
+  {
+    "revision": "3a31b638dad0889f2d95",
+    "url": "/vue-pwa/js/chunk-vendors.bb217372.js"
+  },
+  {
+    "revision": "cdd41bedbe55d0636dc330a535d0d4af",
+    "url": "/vue-pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa/robots.txt"
+  }
+]);
